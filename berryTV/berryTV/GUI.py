0bde803db22ce7109747a4386c88208a589d18b3
Tyson Shannon <129625009+Tyson-Shannon@users.noreply.github.com>
2025-02-15 13:08:24 -0600

    Add files via upload
    add app and settings functions in kinda working order, database added but not yet utilized

---
+++ berryTV/berryTV/GUI.py
@@ -0,0 +1,73 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+import Settings
+import AddApp
+
+class window(QMainWindow):
+   def __init__(self, parent = None):
+      #main screen
+      super(window, self).__init__(parent)
+      self.setWindowTitle("berryTV")
+      self.setStyleSheet("background-color: #FFFFFF;") 
+
+      layout = QVBoxLayout()
+
+      #top bar
+      tb = self.addToolBar("Tool Bar")
+      tb.setStyleSheet("background-color: #5E17EB;")
+      tb.setIconSize(QSize(50, 50))
+      tb.setMovable(False)
+      tb.setFloatable(False)
+      #title display
+      self.title_label = QLabel()
+      title_font = QFont("Classic Console", 30)
+      self.title_label.setFont(title_font) 
+      self.title_label.setStyleSheet("color: #FFFFFF;")
+      tb.addWidget(self.title_label)
+      self.title_label.setText("berryTV")
+      #time display
+      self.time_label = QLabel()
+      time_font = QFont("Arial", 20)
+      self.time_label.setFont(time_font) 
+      self.time_label.setStyleSheet("color: #FFFFFF;")
+      tb.addWidget(self.time_label)
+      self.timer = QTimer()
+      self.timer.timeout.connect(self.update_time)
+      #update every second
+      self.timer.start(1000)
+
+      #side bar
+      sb = self.addToolBar("Side Bar")
+      self.addToolBar(Qt.LeftToolBarArea, sb)
+      sb.setStyleSheet("background-color: #5E17EB;")
+      sb.setIconSize(QSize(50, 50))
+      sb.setMovable(False)
+      sb.setFloatable(False)
+      #buttons
+      add = QAction(QIcon("add.png"), "add app", self)
+      sb.addAction(add)
+      settings = QAction(QIcon("setting.png"), "settings", self)
+      sb.addAction(settings)
+      exit = QAction(QIcon("leave.png"), "exit", self)
+      sb.addAction(exit)
+      sb.actionTriggered[QAction].connect(self.sideBarBut)
+
+      self.setLayout(layout)
+
+   def sideBarBut(self, button):
+      #side bar button actions
+      if button.text() == "exit":
+         self.close()
+      if button.text() == "settings":
+         self.setWindow = Settings.settings()
+         self.setWindow.exec_()
+      if button.text() == "add app":
+         self.addWindow = AddApp.addApp()
+         self.addWindow.exec_()
+   
+   def update_time(self):
+      #update time display
+      current_time = QTime.currentTime().toString("hh:mm:ss")
+      self.time_label.setText(" | "+current_time+" | ")
