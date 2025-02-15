119fcb0c97040ca56143fc041112c153cede7b1b
Tyson Shannon <129625009+Tyson-Shannon@users.noreply.github.com>
2025-02-15 13:09:11 -0600

    Delete berryTV/berryTV directory
    mistake made in push

--- berryTV/berryTV/ActivateVenv.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-def activateVenv(venv_location):
-    # Command to activate the virtual environment
-    activate_script = os.path.join(venv_location, "Scripts", "activate") if os.name == "nt" else os.path.join(venv_location, "bin", "activate")
-
-    # Run the activation and any other commands (if needed)
-    subprocess.call(f"{activate_script}", shell=True)
